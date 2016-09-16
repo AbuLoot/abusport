a388ae0ab4f65f07cca19c9e9c3ed09409659f3f
Issayev Adilet <issayev.adilet@gmail.com>
2016-09-16 17:27:51 +0600

    deleted extra spaces

--- resources/views/board/sports.blade.php
+++ resources/views/board/sports.blade.php
@@ -16,19 +16,19 @@
 
 @section('content')
 
-        <div class="sports">
-          @foreach($sports->sortBy('sort_id')->chunk(4) as $chunk)
-            <div class="row">
-              @foreach($chunk as $sport)
-                <div class="col-md-3 col-sm-3 col-xs-6 sport-item">
-                  <a href="{{ url('sport/'.$sport->slug) }}">
-                    <img class="img-responsive" src="/img/sport/{{ $sport->image }}">
-                    <p class="sport-item-title">{{ $sport->title }}</p>
-                  </a>
-                </div>
-              @endforeach
+    <div class="sports">
+      @foreach($sports->sortBy('sort_id')->chunk(4) as $chunk)
+        <div class="row">
+          @foreach($chunk as $sport)
+            <div class="col-md-3 col-sm-3 col-xs-6 sport-item">
+              <a href="{{ url('sport/'.$sport->slug) }}">
+                <img class="img-responsive" src="/img/sport/{{ $sport->image }}">
+                <p class="sport-item-title">{{ $sport->title }}</p>
+              </a>
             </div>
           @endforeach
         </div>
+      @endforeach
+    </div>
 
 @endsection
